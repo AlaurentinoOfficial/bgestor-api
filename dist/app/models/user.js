1e8a4f6872ec753b8cab957b1095edc5ff482f19
Anderson Laurentino <alaurentino.br@gmail.com>
2018-01-04 01:29:57 -0300

    Add user schema

--- dist/app/models/user.js
+++ dist/app/models/user.js
@@ -1,1 +1,44 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
+var relationship = require("mongoose-relationship");
+
+var userSchema = new mongoose.Schema({
+    solution: { type: mongoose.Schema.ObjectId, ref: "Solution", childPath: "user" },
+    email: { type: String, required: true, lowercase: true, unique: true },
+    password: { type: String, required: true }
+});
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+userSchema.methods.comparePassword = function (pw, cb) {
+    bcrypt.compare(pw, this.password, function (err, isMath) {
+        if (err) return cb(err);
+
+        cb(null, isMath);
+    });
+};
+userSchema.plugin(relationship, { relationshipPathName: 'solution' });
+var UserSchema = exports.UserSchema = mongoose.model('User', userSchema);
---
+++ src/app/models/user.js
@@ -0,0 +1,43 @@
+
+var mongoose = require("mongoose")
+var bcrypt = require("bcrypt")
+var relationship = require("mongoose-relationship")
+
+let userSchema = new mongoose.Schema({
+    solution: {type: mongoose.Schema.ObjectId, ref:"Solution", childPath:"user"},
+    email: {type: String, required: true, lowercase: true, unique: true},
+    password: {type: String, required: true}
+});
+
+userSchema.pre('save', function(next) {
+    let user = this;
+
+    if(this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, (err, salt) => {
+            if(err)
+                return next(err);
+
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if(err)
+                    return next(err);
+                
+                user.password = hash;
+                next();
+            });
+        });
+    }
+    else{
+        return next();
+    }
+});
+
+userSchema.methods.comparePassword = function(pw, cb) {
+    bcrypt.compare(pw, this.password, (err, isMath) => {
+        if(err)
+            return cb(err);
+
+        cb(null, isMath);
+    });
+};
+userSchema.plugin(relationship, { relationshipPathName:'solution' });
+export const UserSchema = mongoose.model('User', userSchema);
