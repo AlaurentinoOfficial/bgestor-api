5994faa664fead0b738a845c13dfbb09177d314c
Anderson Laurentino <alaurentino.br@gmail.com>
2018-01-14 09:19:48 -0300

    Update .gitignore

--- dist/bin/www.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _server = require('../server');
-
-_server.Server.listen(_server.Server.get('port'), function () {
-	console.log('The server is litening in :' + _server.Server.get('port'));
-});
