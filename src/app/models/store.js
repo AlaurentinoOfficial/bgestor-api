3a7f51dbf807779dd0398205e9e5f3126b8195b7
Anderson Laurentino <alaurentino@gmail.com>
2018-05-27 22:24:54 -0300

    Add type for the stores, physical or ecommerce

--- src/app/models/store.js
+++ src/app/models/store.js
@@ -5,6 +5,7 @@ let store = new mongoose.Schema({
     name: {type: String, required: true},
     address: {type: String, required: true},
     ticket: {type: Number, default: 0, required: false},
+    type: {type: String, enum: ['physical', 'ecommerce'], require: true},
     products: [{type: mongoose.Schema.ObjectId, ref:"Product", required: false}],
     sales: [{type: mongoose.Schema.ObjectId, ref:"Sale", required: false}],
     solution: {type: mongoose.Schema.ObjectId, ref:"Solution", childPath:"stores", required: true, unique: false}
