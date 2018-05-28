3453644c0e1195c06737bb68211d1289c8d92319
Anderson Laurentino <alaurentino.br@gmail.com>
2018-05-27 23:16:47 -0300

    Add cors middleware

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser')
 
+import { AllowCrossDomain } from './app/middlewares/cors'
 import { DbConfig, Mongo } from './app/config/database'
 import { Router } from './app/config/router'
 
+app.use(AllowCrossDomain)
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({'extended':'true'}))
 app.use(bodyParser.json())
