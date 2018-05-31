6fc6ca5587786ab2f0dc61a8621f7ccfacc98af4
Anderson Laurentino <alaurentino.br@gmail.com>
2018-05-30 21:56:01 -0300

    Start test

--- src/test.js
+++ src/test.js
@@ -1,10 +1,9 @@
 import * as assert from 'assert'
+import * as axios from 'axios'
 
-describe('Basic Mocha String Test', function () {
-    it('should return number of charachters in a string', function () {
-           assert.equal("Hello".length, 4);
-       });
+describe(bold(green('➜  ') + 'Running tests...'), function () {
+
     it('should return first charachter of the string', function () {
-           assert.equal("Hello".charAt(0), 'H');
-       });
-});
+           assert.equal("Hello".charAt(0), 'H')
+    })
+})
