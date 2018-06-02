5a71604a19097c8b3df4d20b4fc8e2251b3ad195
Anderson Laurentino <alaurentino.br@gmail.com>
2018-06-02 16:16:05 -0300

    Add comments in Product Controller

--- src/app/controllers/product.js
+++ src/app/controllers/product.js
@@ -7,19 +7,25 @@ exports.getAll = (req, res) => {
     if(!req.params.store)
         return res.json({status: false, value: Strings.INVALID_PARAMS})
 
+    // Check if there is that store
     StoreSchema.findOne({_id: req.params.store}, (er, store) => {
         if(er || !store)
             return res.json({status: false, value: Strings.INVALID_PARAMS})
 
+        // Find products
         ProductSchema.find({store: store._id}, (e, products) => {
             if(e || !products)
                 return res.json({status: true, value: []})
 
+            // Get only the attributes
             productsDoc = products._doc
 
+            // Calculate how many days ago of the last recharge
             productsDoc.forEach(p => {
-                if(p.stock_recharge.length > 0) {
-                    p.last_recharge = (new Date() - p.stock_recharge[p.stock_recharge.length-1]) / (24 * 60 * 60 * 1000)
+                const size = p.stock_recharge.length
+
+                if(size > 0) {
+                    p.last_recharge = (new Date() - p.stock_recharge[size-1]) / (24 * 60 * 60 * 1000)
                 }
             })
             
@@ -29,11 +35,15 @@ exports.getAll = (req, res) => {
 }
 
 exports.addNew = (req, res) => {
+    // Check if there is that store
     StoreSchema.findOne({_id: req.params.store}, (er, store) => {
         if(er || !store)
             return res.json({status: false, value: Strings.INVALID_STORE})
         
+        // Add store id in the product 
         req.body.store = store
+
+        // Create a new product
         ProductSchema.create(req.body, (err, products) => {
             if(err)
                 return res.json({status: false, value: Strings.INVALID_PARAMS})
@@ -44,6 +54,7 @@ exports.addNew = (req, res) => {
 }
 
 exports.putById = (req, res) => {
+    // Custom method to update with some restrictions
     ProductSchema.findOneNUpdate({_id: req.params.id}, req.body, (err, p) => {
         if(err)
             return res.json({status: false, value: Strings.INVALID_PARAMS})
@@ -53,6 +64,7 @@ exports.putById = (req, res) => {
 }
 
 exports.addInStockById = (req, res) => {
+    // Update the stock
     ProductSchema.addStock({_id: req.params.id}, req.body.stock, (err, p) => {
         if(err)
             return res.json({status: false, value: Strings.INVALID_PARAMS})
