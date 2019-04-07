1a4e4809e48c5abd2b4f97493e2b7b4cfe3e81a5
Anderson Laurentino <alaurentino.br@gmail.com>
2019-04-07 20:17:19 -0300

    Add time_to_receive in Payment Model

--- src/app/models/payment.js
+++ src/app/models/payment.js
@@ -4,7 +4,8 @@ let payment = new mongoose.Schema({
     solution: {type: mongoose.Schema.ObjectId, ref:"Solution", childPath:"payments", required: true},
 
     name: {type: String, required: true},
-    cost: {type: Number, required: true}
+    cost: {type: Number, required: true},
+    time_to_receive: {type: Number, required: true}
 })
 
 exports.PaymentSchema = mongoose.model('Payment', payment)
