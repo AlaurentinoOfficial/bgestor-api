c00126b4391eed6e94034783e0c335d12e514eeb
Anderson Laurentino <alaurentino.br@gmail.com>
2019-04-07 20:59:17 -0300

    Add Certificate in Solution Model

--- src/app/models/solution.js
+++ src/app/models/solution.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose")
 
 let solution = new mongoose.Schema({
     name: {type: String, required: true, unique: false},
-    cnpj: {type: String, required: true, unique: true}
+    cnpj: {type: String, required: true, unique: true},
+    certificate: {type: String, required: true, unique: true}
 })
 
 exports.SolutionSchema = mongoose.model('Solution', solution)
