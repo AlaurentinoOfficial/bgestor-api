212bacacc807b06f6f90f60141f031b3a54520e6
Anderson Laurentino <alaurentino.br@gmail.com>
2019-04-07 20:43:00 -0300

    Create type enum attr in Taxe Model

--- src/app/models/taxes.js
+++ src/app/models/taxes.js
@@ -4,7 +4,8 @@ let taxe = new mongoose.Schema({
     solution: {type: mongoose.Schema.ObjectId, ref:"Solution", childPath:"taxes", required: true},
 
     name: {type: String, required: true},
-    cost: {type: Number, required: true}
+    cost: {type: Number, required: true},
+    type: {type: String, enum: ['federal', 'state', 'municipal', 'neighborhood', 'other'], require: true}
 })
 
 exports.TaxeSchema = mongoose.model('Taxe', taxe)
