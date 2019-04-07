95de35ebaf048981032195176f73a4f76eaaa3b5
Anderson Laurentino <alaurentino.br@gmail.com>
2019-04-07 20:07:55 -0300

    Validate the Solution id in Sale controller ops

--- src/app/controllers/sale.js
+++ src/app/controllers/sale.js
@@ -3,10 +3,10 @@ import { SaleSchema } from '../models/sale'
 import { Strings } from '../config/strings'
 
 exports.getAll = (req, res) => {
-    StoreSchema.findOne({_id: req.params.store}, (err, store) => {
+    StoreSchema.findOne({solution: res.locals.user.solution, _id: req.params.store}, (err, store) => {
         if(err) return res.json({status: false, value: Strings.INVALID_STORE})
 
-        SaleSchema.find({store: store}, (err, sales) => {
+        SaleSchema.find({solution: res.locals.user.solution, store: store}, (err, sales) => {
             if(err)
                 return res.json({status: true, value: Strings.INTERNAL_ERROR, products: []})
 
@@ -19,7 +19,7 @@ exports.sell = (req, res) => {
     if(!req.body.client || !req.body.products)
         return res.json({status: false, value: Strings.INVALID_PARAMS})
 
-    StoreSchema.findOne({_id: req.params.store}, (err, store) => {
+    StoreSchema.findOne({solution: res.locals.user.solution, _id: req.params.store}, (err, store) => {
         if(err || !store) return res.json({status: false, value: Strings.INVALID_STORE})
 
         req.body.store = store
