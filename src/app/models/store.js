a1b0893cc847b37fbc1966fade720ce2a5798766
Anderson Laurentino <alaurentino.br@gmail.com>
2019-04-07 21:56:26 -0300

    Added the Store Address

--- src/app/models/store.js
+++ src/app/models/store.js
@@ -4,9 +4,16 @@ let store = new mongoose.Schema({
     solution: {type: mongoose.Schema.ObjectId, ref:"Solution", childPath:"stores", required: true},
 
     name: {type: String, required: true},
-    address: {type: String, default: "", required: true},
     type: {type: String, enum: ['physical', 'ecommerce', 'other'], require: true},
     ticket: {type: Number, default: 0, required: false},
+
+    country: {type: String, required: true},
+    state: {type: String, required: true},
+    city: {type: String, required: true},
+    neighborhood: {type: String, required: true},
+    street: {type: String, required: true},
+    number: {type: Number, required: true},
+    complement: {type: String, required: true},
 })
 
 exports.StoreSchema = mongoose.model('Store', store)
