6428cfce19c400f15f6ca07a73edf43d6dce7d19
Anderson Laurentino <alaurentino.br@gmail.com>
2019-04-07 21:34:14 -0300

    Create limits of Products and Stores and Monthly Price in Solution

--- src/app/models/solution.js
+++ src/app/models/solution.js
@@ -3,6 +3,9 @@ var mongoose = require("mongoose")
 let solution = new mongoose.Schema({
     name: {type: String, required: true, unique: false},
     cnpj: {type: String, required: true, unique: true},
+    stores_limit: {type: Number, required: true},
+    products_limit: {type: Number, required: true},
+    monthly_price: {type: Number, required: true},
     certificate: {type: String, required: true, unique: true}
 })
 
