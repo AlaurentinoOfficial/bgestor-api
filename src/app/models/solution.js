339194d5ac2f4600858468b0ab600440a06e1fe2
Anderson Laurentino <alaurentino.br@gmail.com>
2019-04-07 21:55:53 -0300

    Added the Business address

--- src/app/models/solution.js
+++ src/app/models/solution.js
@@ -3,6 +3,15 @@ var mongoose = require("mongoose")
 let solution = new mongoose.Schema({
     name: {type: String, required: true, unique: false},
     cnpj: {type: String, required: true, unique: true},
+
+    country: {type: String, required: true},
+    state: {type: String, required: true},
+    city: {type: String, required: true},
+    neighborhood: {type: String, required: true},
+    street: {type: String, required: true},
+    number: {type: Number, required: true},
+    complement: {type: String, required: true},
+
     stores_limit: {type: Number, required: true},
     products_limit: {type: Number, required: true},
     monthly_price: {type: Number, required: true},
